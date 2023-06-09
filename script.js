cde14b780cbcd748dcfef7e0894b93b050257541
Lucas Andrade Torres <127760173+Andrade098@users.noreply.github.com>
2023-06-09 11:18:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Developer Web Full Stack", "Front End", "Bancos de Dados", 
+    "Back End"],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
